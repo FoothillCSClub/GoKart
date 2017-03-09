faa8a2557f5fb95c0353e41150df78f7992e33be
TryExceptElse <SchwarzJohnV@gmail.com>
2017-03-08 19:17:46 -0800

    added DepthMap and PointCloud classes to Kinect module, wrapping depth_arr and points_arr respectively

--- kart/Kinect-Edge-Detection/pm/kinect.py
+++ kart/Kinect-Edge-Detection/pm/kinect.py
@@ -44,7 +44,7 @@ class KinGeo:
         # assert self.device is not None
         self.last_access = 0.  # time of last kinect depth map access
         self._frame_time = 1./KinGeo.default_max_frq  # min time in s per frame
-        self._depth_arr = None  # holds numpy array of
+        self._depth_map = None  # holds numpy array of
         self._pc_timestamp = 0.
         self._points_arr_timestamp = 0.
 
@@ -76,88 +76,89 @@ class KinGeo:
     @property
     def depth_map(self):
         """
-        Gets depth map as numpy array.
-        :return: np.Array
+        Gets DepthMap for current sensor frame
+        :return: DepthMap
         """
         # If elapsed time since last frame is greater than frame
         # rate, update depth map.
         if self.t_since_last_frame > self._frame_time:
             # get depth map from first Kinect found
-            self._depth_arr = fn.sync_get_depth()
-            if not self._depth_arr:
+            self._depth_map = fn.sync_get_depth()
+            if not self._depth_map:
                 raise OSError('Could not connect to Kinect')
+            self.last_access = t.time()
         # Return the depth-map portion of the depth map array.
         # The second part of the array is the timestamp.
-        return self._depth_arr[0]
+        return DepthMap(self._depth_map)
 
     @property
-    def points_arr(self):
+    def point_cloud(self):
         """
         Gets point cloud of positions from depth map.
         Returns point cloud as three dimensional array of
         [column][row][point position]
         :return: np.Array
         """
-        dm = self.depth_map  # get depth map (a 2d numpy array)
-        cloud_height = math.ceil(SENSOR_PIXEL_HEIGHT / SAMPLE_DISTANCE)
-        cloud_width = math.ceil(SENSOR_PIXEL_WIDTH / SAMPLE_DISTANCE)
-        # make array that point positions will be stored in
-        points = np.ndarray((cloud_height, cloud_width, 3), np.float32)
-        # for all combinations of x and y...
-        x_range = range(0, SENSOR_PIXEL_WIDTH, SAMPLE_DISTANCE)
-        y_range = range(0, SENSOR_PIXEL_HEIGHT, SAMPLE_DISTANCE)
-        for x, y in itr.product(x_range, y_range):
-            # create a point in the newly formed point-cloud.
-            map_depth = dm[y][x]
-            arr_x_index = int(x / SAMPLE_DISTANCE)
-            arr_y_index = int(y / SAMPLE_DISTANCE)
-            if map_depth == 2047:
-                # if depth is max value, set marker value and go on
-                points[arr_y_index][arr_x_index] = (0, 0, 0)
-                continue
-            points[arr_y_index][arr_x_index] = \
-                pos_from_depth_map_point(x, y, map_depth)
-        return points
+        # build point cloud from current depth map
+        return self.depth_map.point_cloud
 
     @property
-    def traversable_points(self):
-        """
-        Returns array of booleans indicating whether the point at the
-        same position in point_arr is able to be traversed
-        At the moment, simply tests the angle of the slope between
-        one point and the next in the column, and if the slope is
-        greater than maximum value set in constant, sets that position
-        to false in the array.
-        Points in point_arr that are empty should not be accessed,
-        and therefor do not have a value set.
-        :return: np.array(boolean)
-        """
-        points = self.points_arr
-        bools = np.ndarray(points.shape[0], points.shape[1])  # make bool array
-        x_range = range(1, points.shape[1])
-        y_range = range(1, points.shape[0])
-        for x, y in itr.product(x_range, y_range):  # for each x, y combo
-            bools[x][y] = \
-                slope_in_bounds(points[y][x], points[y - 1][x]) and \
-                slope_in_bounds(points[y][x], points[y][x - 1])
-        return bools
+    def points_arr(self):
+        return self.point_cloud.point_arr
 
 
-def blurred_depth(depth_map, radius, x, y) -> float:
-    x_range = range(x - radius, x + radius)
-    y_range = range(y - radius, y + radius)
-    blur_positions = [(x, y) for x, y in itr.product(x_range, y_range)]
-    depth_sum = 0.
-    n_depths_summed = 0
-    for position in blur_positions:
-        x, y = position
-        if 0 <= x < SENSOR_PIXEL_WIDTH and 0 <= y < SENSOR_PIXEL_HEIGHT:
-            depth_at_pos = depth_map[y][x]
-            if depth_at_pos != 2047:
-                depth_sum += depth_at_pos
-                n_depths_summed += 1
-    average_depth = depth_sum / n_depths_summed if n_depths_summed else 2047
-    return average_depth
+class DepthMap:
+    def __init__(self, arr):
+        self.arr = arr
+        self._point_cloud = None
+
+    @property
+    def time_stamp(self):
+        return self.arr[1]
+
+    @property
+    def point_cloud(self) -> 'Point':
+        if not self._point_cloud:
+            self._point_cloud = PointCloud(self.arr)
+        return self._point_cloud
+
+
+class PointCloud:
+    def __init__(self, depth_arr):
+        self.depth_arr = depth_arr
+        self._point_arr = None
+
+    @property
+    def point_arr(self):
+        if not self._point_arr:
+            self._point_arr = point_arr_from_depth_arr(self.depth_arr[0])
+        return self._point_arr
+
+    @property
+    def time_stamp(self) -> int:
+        return self.depth_arr[1]
+
+
+def point_arr_from_depth_arr(dm):
+    cloud_height = math.ceil(SENSOR_PIXEL_HEIGHT / SAMPLE_DISTANCE)
+    cloud_width = math.ceil(SENSOR_PIXEL_WIDTH / SAMPLE_DISTANCE)
+    # make array that point positions will be stored in
+    points = np.ndarray((cloud_height, cloud_width, 3), np.float32)
+    # for all combinations of x and y...
+    x_range = range(0, SENSOR_PIXEL_WIDTH, SAMPLE_DISTANCE)
+    y_range = range(0, SENSOR_PIXEL_HEIGHT, SAMPLE_DISTANCE)
+    for x, y in itr.product(x_range, y_range):
+        # create a point in the newly formed point-cloud.
+        map_depth = dm[y][x]
+        arr_x_index = int(x / SAMPLE_DISTANCE)
+        arr_y_index = int(y / SAMPLE_DISTANCE)
+        if map_depth == 2047:
+            # if depth is max value, set marker value and go on
+            points[arr_y_index][arr_x_index] = (0, 0, 0)
+            continue
+        points[arr_y_index][arr_x_index] = \
+            pos_from_depth_map_point(x, y, map_depth)
+    return points
 
 
 def pos_from_depth_map_point(x, y, map_depth):
@@ -166,7 +167,7 @@ def pos_from_depth_map_point(x, y, map_depth):
     angular_y = (y - HALF_SENSOR_PX_HEIGHT) / SENSOR_PIXEL_HEIGHT * \
         SENSOR_ANGULAR_HEIGHT + SENSOR_ANGULAR_ELEVATION
     # 0.1236
-    depth = np.tan(map_depth / 2842.5 + 1.1863)  # ????
+    depth = np.tan(map_depth / 2842.5 + 1.1863)
     pos = (
         math.sin(angular_x) * depth,
         depth,
@@ -188,11 +189,3 @@ def slope_in_bounds(p1, p2):
     v_difference_sq = np.power(dif[1], 2)
     slope_sq = v_difference_sq / flat_distance_sq
     return slope_sq < SLOPE_COMPARISON_VAL
-
-if __name__ == '__main__':
-    kin_geo = KinGeo()  # create kinect handler obj
-    test_dm = kin_geo.depth_map
-    print(len(test_dm))
-    print(len(test_dm[0]))
-    print(len(test_dm[0][0]))
-    print(test_dm[1])
