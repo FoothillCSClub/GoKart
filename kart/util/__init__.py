2ddbe956a35a88bb5dd54d0034611afb3af3b236
TryExceptElse <SchwarzJohnV@gmail.com>
2017-03-12 20:00:34 -0700

    replaced docstring for util package

--- kart/util/__init__.py
+++ kart/util/__init__.py
@@ -1,5 +1,4 @@
 """
 This module is here to store utility classes that will be used between
-classes; for example, Points, which will be generated in input,
-stored in drive_data, and used in drive_logic packages.
+classes.
 """
