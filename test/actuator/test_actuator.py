0cfd63dd169d223a7c9be92e01e4abd9a20c8124
TryExceptElse <SchwarzJohnV@gmail.com>
2017-05-17 10:01:27 -0700

    added duty cycle tests to test_actuator

--- test/actuator/test_actuator.py
+++ test/actuator/test_actuator.py
@@ -20,13 +20,15 @@ class TestActuator(TestCase):
         if chan_num == 2:
             return self.chan2
 
+    def mock_duty_cycle_setter(self, pulse_width: float):
+        self.pulse_width = pulse_width
+
     def setUp(self):
-        self.chan0 = Mock(name='speed_chan (0)')
-        self.chan1 = Mock(name='steer_dir_chan (1)')
-        self.chan2 = Mock(name='steer_mag_chan (2)')
+        self.chan0 = self.MockPwmChannel(name='speed_chan (0)')
+        self.chan1 = self.MockPwmChannel(name='steer_dir_chan (1)')
+        self.chan2 = self.MockPwmChannel(name='steer_mag_chan (2)')
 
     def test_that_setting_speed_0_results_in_speed_pulse_width_0(self):
-
         mock_pwm = Mock(name='mock_pwm')
         mock_pwm.get_channel = self.mock_channel_getter
         a = Actuator(None, mock_pwm)  # data instance will not be used
@@ -34,9 +36,38 @@ class TestActuator(TestCase):
         self.chan0.set_duty_cycle.assert_called_with(0)
 
     def test_setting_max_speed_results_in_speed_pulse_width_1(self):
-
         mock_pwm = Mock(name='mock_pwm')
         mock_pwm.get_channel = self.mock_channel_getter
         a = Actuator(None, mock_pwm)  # data instance will not be used
         a.speed = MAX_SPEED
         self.chan0.set_duty_cycle.assert_called_with(1)
+
+    def test_calling_stop_wheel_turn_results_in_steer_mag_pw_of_0(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.stop_wheel_turn()
+        self.chan2.set_duty_cycle.assert_called_with(0)
+
+    def test_calling_turn_wheels_right_results_in_correct_direction_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels_right()
+        self.chan1.set_duty_cycle.assert_called_with(1)
+
+    def test_calling_turn_wheels_left_results_in_correct_direction_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels_left()
+        self.chan1.set_duty_cycle.assert_called_with(0)
+
+    class MockPwmChannel(Mock):
+        @property
+        def duty_cycle(self):
+            return None
+
+        @duty_cycle.setter
+        def duty_cycle(self, duty_cycle):
+            self.set_duty_cycle(duty_cycle)
