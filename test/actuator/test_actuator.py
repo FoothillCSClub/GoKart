f5e3e47878f68f8545f9fdba17d8fb39325f2651
TryExceptElse <SchwarzJohnV@gmail.com>
2017-07-23 16:01:28 -0700

    added a bunch of little tests for Actuator.turn_wheels() method.

--- test/actuator/test_actuator.py
+++ test/actuator/test_actuator.py
@@ -25,8 +25,10 @@ class TestActuator(TestCase):
 
     def setUp(self):
         self.chan0 = self.MockPwmChannel(name='speed_chan (0)')
-        self.chan1 = self.MockPwmChannel(name='steer_dir_chan (1)')
+        self.chan1 = self.MockPwmChannel(name='steer_dir_chan (1)')  # 0 = left
         self.chan2 = self.MockPwmChannel(name='steer_mag_chan (2)')
+
+    # SPEED PWM TESTS
 
     def test_that_setting_speed_0_results_in_speed_pulse_width_0(self):
         mock_pwm = Mock(name='mock_pwm')
@@ -41,6 +43,8 @@ class TestActuator(TestCase):
         a = Actuator(None, mock_pwm)  # data instance will not be used
         a.speed = MAX_SPEED
         self.chan0.set_duty_cycle.assert_called_with(1)
+
+    # WHEEL TURN PWM TESTS
 
     def test_calling_stop_wheel_turn_results_in_steer_mag_pw_of_0(self):
         mock_pwm = Mock(name='mock_pwm')
@@ -62,6 +66,73 @@ class TestActuator(TestCase):
         a = Actuator(None, mock_pwm)  # data instance will not be used
         a.turn_wheels_left()
         self.chan1.set_duty_cycle.assert_called_with(0)
+
+    def test_calling_turn_wheels_with_0_radius_results_in_correct_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(0)
+        self.chan2.set_duty_cycle.assert_called_with(0)
+
+    def test_calling_turn_wheels_with_left_value_results_in_correct_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(-1)
+        self.chan1.set_duty_cycle.assert_called_with(0)
+        self.chan2.set_duty_cycle.assert_called_with(1)
+
+    def test_calling_turn_wheels_with_right_value_results_in_correct_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(1)
+        self.chan1.set_duty_cycle.assert_called_with(1)
+        self.chan2.set_duty_cycle.assert_called_with(1)
+
+    def test_calling_turn_wheels_w_right_fraction_results_in_correct_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(0.5)
+        self.chan1.set_duty_cycle.assert_called_with(1)
+        self.chan2.set_duty_cycle.assert_called_with(0.5)
+
+    def test_calling_turn_wheels_w_left_fraction_results_in_correct_pw(self):
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(-0.75)
+        self.chan1.set_duty_cycle.assert_called_with(0)
+        self.chan2.set_duty_cycle.assert_called_with(0.75)
+
+    def test_calling_turn_wheels_with_val_gt_1_results_in_correct_pw(self):
+        """
+        Test that calling the turn_wheels method with a value greater
+        than 1 results in magnitude channel only being passed a
+        value of 1.
+        """
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(1.89)
+        self.chan1.set_duty_cycle.assert_called_with(1)
+        self.chan2.set_duty_cycle.assert_called_with(1)
+
+    def test_calling_turn_wheels_with_val_lt_neg_1_results_in_correct_pw(self):
+        """
+        Test that calling the turn_wheels method with a value less than
+        -1 results in magnitude channel only being passed a
+        value of -1.
+        """
+        mock_pwm = Mock(name='mock_pwm')
+        mock_pwm.get_channel = self.mock_channel_getter
+        a = Actuator(None, mock_pwm)  # data instance will not be used
+        a.turn_wheels(-15.7)
+        self.chan1.set_duty_cycle.assert_called_with(0)
+        self.chan2.set_duty_cycle.assert_called_with(1)
+
+    # RADIUS TO WHEEL ANGLE TESTS
 
     def test_radius_to_wheel_angle_returns_correctly_at_10m_right_radius(self):
         # check that physical constants have not changed. If they
@@ -106,6 +177,8 @@ class TestActuator(TestCase):
         a = Actuator(None, mock_pwm)  # data instance will not be used
         # assert floats are equal to 7 places
         self.assertAlmostEquals(-0.205075900383, a._radius_to_wheel_angle(-5))
+
+    # HELPER CLASSES
 
     class MockPwmChannel(Mock):
         @property
